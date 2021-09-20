1fb34383e656cc17544d7aa36b652d02ee8b897a
BRIAN MCCANN <extern.mccann@mbusa.com>
2021-09-20 07:00:52 -0400

    copy pastaed a stepper component working on customization

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -7,10 +7,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./stepper.component.css']
 })
 export class StepperComponent implements OnInit {
-
-
+  currentStep = 1
+  stepOneState = 'tab-pane active';
+  stepTwoState = 'tab-pane fade'
   ngOnInit() {
 
   }
 
+  // advanceStep(stepToGoTo: number) {
+  //   console.log("advance called with ", stepToGoTo);
+  //   this.currentStep = stepToGoTo;
+  // }
+  shouldShowStep(stepNo: number) {
+    return this.currentStep == stepNo;
+  }
+  advanceButtonClicked() {
+    this.currentStep++;
+    console.log('currentStep :>> ', this.currentStep);
+    this.stepOneState = 'tab-pane fade';
+    this.stepTwoState = 'tab-pane active'
+  }
+  // stepOneState(){
+  //   return ['tab-pane', 'active']
+  // }
+  // stepTwoState(){
+  //   console.log('step two state called');
+  //   if(this.currentStep == 2){
+  //     return ['tab-pane', 'active']
+  //   }else{
+  //     return 'tab-pane'
+  //   }
+  // }
 }
