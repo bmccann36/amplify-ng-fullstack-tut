3ff989f260d0ab14fb3fe4b3dfa23670919a220a
mccann-b <brian@cedrus.digital>
2021-10-18 08:38:09 -0400

    made it so correct error code shows when user can't sign in scaffold html for sign up comp

--- src/app/signup-modal/signup-modal.component.ts
+++ src/app/signup-modal/signup-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-signup-modal',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-modal.component.scss']
 })
 export class SignupModalComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    public bsModalRef: BsModalRef,
+  ) { }
 
-  constructor() { }
+  signUpForm = this.formBuilder.group({
+    email: '',
+    password: ''
+  });
 
   ngOnInit(): void {
+  }
+
+  async onSubmit(){
+
   }
 
 }
